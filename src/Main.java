0b96d74c8c1b0c308db2930c2f586756edb0bf92
buseeyldrm <105048165+buseeyldrm@users.noreply.github.com>
2022-12-02 16:58:27 +0300

    yıldızlar ile harf yazma

---
+++ src/Main.java
@@ -0,0 +1,65 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        //A HARFİNİ  YILDIZLAR YAZMA
+
+     /*   String[][] harf=new String[6][4];
+
+        for (int i = 0; i < harf.length ; i++) {
+            for (int j = 0; j <harf[i].length ; j++) {
+                if (i==0 || i==2){
+                    harf[i][j]=" * ";
+
+                } else if (j==0 || j==3) {
+                    harf[i][j]=" * ";
+                    
+                }
+                else {
+                    harf[i][j]="   ";
+                }
+
+            }
+
+        }
+        for (String[] row: harf){
+            for (String col: row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+      */
+
+
+        //B HARFİNİ YILDIZLAR İLE YAZMA:
+
+        String [][] harf=new String[7][4];
+
+
+        for (int i = 0; i < harf.length ; i++) {
+            for (int j = 0; j <harf[i].length ; j++) {
+                if (i==0 || i==3 || i==6){
+                    harf[i][j]=" * ";
+
+                } else if (j==0 || j==3) {
+                    harf[i][j]=" * ";
+
+                }
+                else {
+                    harf[i][j]="   ";
+                }
+
+            }
+
+        }
+        for (String [] row:harf){
+            for (String col: row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+    }
+}
